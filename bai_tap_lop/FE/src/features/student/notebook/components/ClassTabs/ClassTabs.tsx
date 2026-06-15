dd87f78ddfa04151a4dbc502e9b86203bbe7e60e
HuanNguyen454 <nhuan454@gmail.com>
2026-06-15 09:43:19 +0700

    Minor fix UI

--- bai_tap_lop/FE/src/features/student/notebook/components/ClassTabs/ClassTabs.tsx
+++ bai_tap_lop/FE/src/features/student/notebook/components/ClassTabs/ClassTabs.tsx
@@ -36,7 +36,7 @@ export function ClassTabs({ activeClassKey, items, onClassChange, onNextClass, o
                   <span className={`${isActive ? "text-xl text-blue-800" : "text-lg text-black"} font-handwriting`}>
                     {classItem.name}
                   </span>
-                  {isActive && <span className="text-yellow-400">★</span>}
+                  {isActive && <span className="text-black">★</span>}
                 </div>
                 <span className={`text-xs font-medium ${isActive ? "text-green-700" : "text-gray-500"}`}>
                   {classItem.teacher}
